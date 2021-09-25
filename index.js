aff69c29fc33d27165b441de9b57546cafcbb5ca
Jyoti Ranjan Kalta <jyotiranjankalta81@gmail.com>
2021-09-25 15:30:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	res.send('Running');
+});
+
+if ( process.env.NODE_ENV == "production"){
+
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+
+}
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
